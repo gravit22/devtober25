4b5343e1c3b2eee516ae1dd821ea1f011f9ce223
Hartimis <Jonathanhart16@gmail.com>
2025-09-30 13:11:29 -0600

    Health UI Update

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: devtober25
 - Unity version: Unity 6000.0.44f1
 - Active game object:
-  - Name: FrountHealthBar
+  - Name: Crosshair
   - Tag: Untagged
   - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Hartimis's Folder/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float health;
+    private float lerpTimer;
+    public float maxHealth = 100f;
+    public float chipSpeed = 2f;
+    public Image frountHealthBar;
+    public Image backHealthBar;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0 , maxHealth);
+        UpdateHealthUI();
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            TakeDamage(Random.Range(5, 10));
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            RestoreHealth(Random.Range(5, 10));
+        }
+    }
+
+    public void UpdateHealthUI()
+    {
+        Debug.Log(health);
+        float fillF = frountHealthBar.fillAmount;
+        float fillB = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth;
+        if (fillB > hFraction)
+        {
+            frountHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentCompleat = lerpTimer / chipSpeed;
+            percentCompleat = percentCompleat * percentCompleat;
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentCompleat);
+        }
+        if (fillF < hFraction)
+        {
+            backHealthBar.color = Color.green;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentCompleat = lerpTimer / chipSpeed;
+            percentCompleat = percentCompleat * percentCompleat;
+            frountHealthBar.fillAmount = Mathf.Lerp(fillF, backHealthBar.fillAmount, percentCompleat);
+        }
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+    }
+    
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+    }
+
+}
